bac4cdc34af717a25f2b8535f68f161a2f1b90e3
Kimena <realarinzemmanuel@gmail.com>
2023-06-19 14:21:05 +0100

    Update and rename cards.js to variables.js

---
+++ variables.js
@@ -0,0 +1,52 @@
+export let startInfo = ` <p class="heading h2">NAIJA WHOT</p>
+<p class="sub-heading h3">
+  Hi! I'm Joe. Would you like to play a game of Naija Whot with me?
+</p>
+<div></div>
+<div class="d-flex justify-content-center align-items-center gap-2"
+>
+  <button type="button" class="btn btn-outline-light ok-btn">Play</button>
+  <button type="button" class="btn btn-outline-light cancel-btn">
+    Pass
+  </button>
+</div>`;
+
+export let computerWinInfo = `<p class="heading h2">JOE WON!</p>
+<p class="sub-heading h3">
+  Yay!! I'm Joe, and I won. Better luck next time.
+</p>
+<div></div>
+<div class="d-flex justify-content-center align-items-center gap-2"
+>
+  <button type="button" class="btn btn-outline-light ok-btn">Play Again</button>
+  <button type="button" class="btn btn-outline-light cancel-btn">
+    Pass
+  </button>
+</div>`;
+
+export let playerWinInfo = `<p class="heading h2">YOU WON!</p>
+<p class="sub-heading h3">
+  Hey, I'm Joe. I'm programmed to be happy for you.
+</p>
+<div></div>
+<div class="d-flex justify-content-center align-items-center gap-2"
+>
+  <button type="button" class="btn btn-outline-light ok-btn">Play Again</button>
+  <button type="button" class="btn btn-outline-light cancel-btn">
+    Pass
+  </button>
+</div>`;
+
+export let restartOrPass = `<p class="heading h2">NAIJA WHOT</p>
+<p class="sub-heading h3">
+  Hey, I'm Joe. Something wrong?
+</p>
+<div></div>
+<div class="d-flex justify-content-center align-items-center gap-2"
+>
+  <button type="button" class="btn btn-outline-light ok-btn">Restart</button>
+  <button type="button" class="btn btn-outline-light exit-btn">
+    Exit
+  </button>
+  <button type="button" class="btn btn-outline-light cancel-btn">Cancel</button>
+</div>`;
