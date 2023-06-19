60687cbd5942fdc02edf93735b86e3b9928831be
Kimena <realarinzemmanuel@gmail.com>
2023-06-20 00:28:51 +0100

    Update variables.js

--- variables.js
+++ variables.js
@@ -3,7 +3,7 @@ export let startInfo = ` <p class="heading h2">NAIJA WHOT</p>
   Hi! I'm Joe. Would you like to play a game of Naija Whot with me?
 </p>
 <div></div>
-<div class="d-flex justify-content-center align-items-center gap-2"
+<div class="d-flex justify-content-center align-items-center"
 >
   <button type="button" class="btn btn-outline-light ok-btn">Play</button>
   <button type="button" class="btn btn-outline-light cancel-btn">
@@ -16,7 +16,7 @@ export let computerWinInfo = `<p class="heading h2">JOE WON!</p>
   Yay!! I'm Joe, and I won. Better luck next time.
 </p>
 <div></div>
-<div class="d-flex justify-content-center align-items-center gap-2"
+<div class="d-flex justify-content-center align-items-center"
 >
   <button type="button" class="btn btn-outline-light ok-btn">Play Again</button>
   <button type="button" class="btn btn-outline-light cancel-btn">
@@ -29,7 +29,7 @@ export let playerWinInfo = `<p class="heading h2">YOU WON!</p>
   Hey, I'm Joe. I'm programmed to be happy for you.
 </p>
 <div></div>
-<div class="d-flex justify-content-center align-items-center gap-2"
+<div class="d-flex justify-content-center align-items-center"
 >
   <button type="button" class="btn btn-outline-light ok-btn">Play Again</button>
   <button type="button" class="btn btn-outline-light cancel-btn">
@@ -42,7 +42,7 @@ export let restartOrPass = `<p class="heading h2">NAIJA WHOT</p>
   Hey, I'm Joe. Something wrong?
 </p>
 <div></div>
-<div class="d-flex justify-content-center align-items-center gap-2"
+<div class="d-flex justify-content-center align-items-center"
 >
   <button type="button" class="btn btn-outline-light ok-btn">Restart</button>
   <button type="button" class="btn btn-outline-light exit-btn">
